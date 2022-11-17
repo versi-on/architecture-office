b7dc5be08c54c6802ced8282b823de5820f95db1
Natalia <adelina09715@gmail.com>
2022-11-17 22:48:11 +0100

    add routes to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import AllRoutes from './AllRoutes';
+import Header from './components/Header';
 import './assets/style/app.css';
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App wrapper">
       <BrowserRouter>
+        <Header />
         <AllRoutes />
       </BrowserRouter>
     </div>
