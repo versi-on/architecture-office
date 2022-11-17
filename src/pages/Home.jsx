d2e5556a74a207b4f672a7a60812ef2ac6883b7e
Natalia <adelina09715@gmail.com>
2022-11-17 22:47:28 +0100

    add components to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import Hero from '../components/Hero'
+import News from '../components/News'
+import Projects from '../components/Projects'
+
 export default function Home() {
     return (
-        <div>Home</div>
+        <div className='home column'>
+            <Hero />
+            <News />
+            <Projects />
+        </div>
     )
 }
