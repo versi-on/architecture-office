2b248190ef2fb2e3809994bed64b8bd41f3a4d7f
Natalia <adelina09715@gmail.com>
2022-11-23 13:47:35 +0100

    add contact

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
+import Contact from '../components/Contact'
 import Hero from '../components/Hero'
 import News from '../components/News'
 import Projects from '../components/Projects'
+import Concept from './Concept'
 
 export default function Home() {
     return (
@@ -8,6 +10,8 @@ export default function Home() {
             <Hero />
             <News />
             <Projects />
+            <Concept />
+            <Contact />
         </div>
     )
 }
